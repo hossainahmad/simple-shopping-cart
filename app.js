d23cd48151aaa78c368ffd024451d1ef4932fda4
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-13 19:29:02 +0600

    DRY principle added & added in function

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-document.getElementById('case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncreasing){
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    if(isIncreasing == true){
+        caseInput.value = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+}
+
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) - 1;
-})
+    updateCaseNumber(false);
+});
