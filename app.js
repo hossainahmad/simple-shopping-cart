66ff45f270d3507637ce3654aab807510c8d766e
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-14 21:02:22 +0600

    total sub-total and Tax calculation added with function

--- app.js
+++ app.js
@@ -8,10 +8,31 @@ function updateProductNumber(product, price, isIncreasing){
         ProductNumber = parseInt(ProductNumber) - 1;
     }
     ProductInput.value = ProductNumber;
-
+    // Update Total
     const ProductTotal = document.getElementById(product + '-total');
     ProductTotal.innerText = ProductNumber * price;
+
+    // Calculate Total
+    calculateTotal();  
 }
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxAmount = subTotal / 10;
+    const totalPrice = subTotal + taxAmount;
+    
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = taxAmount;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+    // Phone Increase Decrease Events
 
 document.getElementById('phone-plus').addEventListener('click', function(){
     updateProductNumber('phone', 1219, true);
@@ -20,6 +41,8 @@ document.getElementById('phone-plus').addEventListener('click', function(){
 document.getElementById('phone-minus').addEventListener('click', function(){
     updateProductNumber('phone', 1219, false)
 });
+
+    // Case Increase Decrease Events
 
 document.getElementById('case-plus').addEventListener('click', function(){
     updateProductNumber('case', 59, true);
