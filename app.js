f4e87553517fc3f4b91be1d6ba8a8e50355ed809
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-14 20:25:59 +0600

    product calculation updated with plus and minus

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing){
+    const ProductInput = document.getElementById(product + '-number');
+    let ProductNumber = ProductInput.value;
     if(isIncreasing == true){
-        caseInput.value = parseInt(caseNumber) + 1;
+        ProductNumber = parseInt(ProductNumber) + 1;
     }
-    else if(caseNumber > 0){
-        caseInput.value = parseInt(caseNumber) - 1;
+    else if(ProductNumber > 0){
+        ProductNumber = parseInt(ProductNumber) - 1;
     }
+    ProductInput.value = ProductNumber;
+
+    const ProductTotal = document.getElementById(product + '-total');
+    ProductTotal.innerText = ProductNumber * price;
 }
 
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, true);
+});
+
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, false)
+});
+
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 });
